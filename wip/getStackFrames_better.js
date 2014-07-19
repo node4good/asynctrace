c55eb0c448d2a6aaf6298e9f2bd530669f47832c
Refael Ackermann <refael@empeeric.com>
2014-07-19 22:25:06 +0300

    Minimum noise

--- wip/getStackFrames_better.js
+++ wip/getStackFrames_better.js
@@ -1,16 +1,7 @@
 'use strict';
-// just Prettify
-var collectStackTrace, functionGetPositionForOffset, functionGetScript;
-/* jshint ignore:start */
-collectStackTrace = global['%CollectStackTrace'];
-functionGetPositionForOffset = global['%FunctionGetPositionForOffset'];
-functionGetScript = global['%FunctionGetScript'];
-/* jshint ignore:end */
-
-
 function getRawFrames(otp) {
     var err = new Error(otp);
-    var raw_stack = collectStackTrace(err, otp||getStackFrames, 100);
+    var raw_stack = %CollectStackTrace(err, otp||getRawFrames, 100); // jshint ignore:line
     var sloppy_frames_count = raw_stack[0];
     var raw_frames = new Array(sloppy_frames_count);
     for (var i = 1; i < raw_stack.length; i += 4) {
@@ -30,8 +21,8 @@ function getStackFrames_better(otp) {
     var frames = [];
     while (raw_frames.length) {
         var frame = raw_frames.unshift();
-        var rawPos = functionGetPositionForOffset(frame.code, frame.positionCounter);
-        var script = functionGetScript(frame.func);
+        var rawPos = %FunctionGetPositionForOffset(frame.code, frame.positionCounter); // jshint ignore:line
+        var script = %FunctionGetScript(frame.func); // jshint ignore:line
         var scriptLocation = script.locationFromPosition(rawPos);
         var file = script.nameOrSourceURL();
         var item = {receiver: frame.receiver.name, name: frame.func.name, file: file, line: scriptLocation.line, column:scriptLocation.column};
