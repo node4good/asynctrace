60c4c7f6000f020b61e3fc3591eb70b135103a01
Refael Ackermann <refael@empeeric.com>
2015-10-06 14:41:56 -0400

    dep bump, back to trusting published async-trace

--- test/common.js
+++ test/common.js
@@ -2,8 +2,7 @@
 var fs = require('fs');
 var fd = fs.openSync("raw_out.log", 'a');
 function writeSync(str) {
-    var buffer = new Buffer(str, 'utf8');
-    fs.writeSync(process.stdout.fd, buffer, 0, buffer.length, null);
-    fs.writeSync(fd, buffer, 0, buffer.length, null);
+    console._stdout.write(str);
+    fs.write(fd, str);
 }
 global.log = writeSync;
